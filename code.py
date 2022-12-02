afaf6d7a1e4bbe8080b71d856d754a38ac39c482
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2022-12-02 09:23:35 +0530

    7thcommit

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # Function to print binary number using recursion
 def convertToBinary(n):
    if n > 3:
-       convertToBinary(n//2)
+       convertToBinary(n//3)
    print(n % 4,end = '')
 
 # decimal number
